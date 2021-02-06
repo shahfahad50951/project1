c10bccbf731e7859cb32347878ed5b8301c29c70
Shah Fahad <shahfahad.50951@gmail.com>
2021-02-06 23:48:20 +0530

    New Feature: Subtraction

--- main.c
+++ main.c
@@ -2,11 +2,18 @@
 #include "add.c"
 int main()
 {
-	int a = 0, b = 0;
+	int a = 0, b = 0, ch = 0;
 	printf("Enter first number: ");
 	scanf("%d", &a);
 	printf("Enter second number: ");
 	scanf("%d", &b);
-	printf("The sum of these 2 numbers is: %d\n", add(a, b));
+	printf("1: Add the 2 numbers\n2: Subtract the 2 numbers\nEnter your choice: ");
+	scanf("%d", &ch);
+	if (ch == 1)
+		printf("The sum of these 2 numbers is: %d\n", add(a, b));
+	else if(ch == 2)
+		printf("The value of subtraction of first from second is: %d\n", subtract(a, b));
+	else 
+		printf("Invalide choice\n");
 	return 0;
 }

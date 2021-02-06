18c6218cdbd056b1858478325141f712adf55419
Shah Fahad <shahfahad.50951@gmail.com>
2021-02-06 23:40:20 +0530

    Added a feature of addition to main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 int main()
 {
 	int a = 0, b = 0;
-	printf("Enter a number: ");
+	printf("Enter first number: ");
 	scanf("%d", &a);
 	printf("Enter second number: ");
 	scanf("%d", &b);
-	printf("The sum of these 2 numbers is: %d", add(a, b));
+	printf("The sum of these 2 numbers is: %d\n", add(a, b));
 	return 0;
 }

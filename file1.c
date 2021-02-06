ecae92cb39f8f536704ab164fdb0e3d03bf739a2
Shah Fahad <shahfahad.50951@gmail.com>
2021-02-06 23:25:53 +0530

    Fixed another bug in file1.c

--- file1.c
+++ file1.c
@@ -2,9 +2,9 @@
 int main(int argc, char *argv[])
 {
 	if(argc > 1)
-		while(agrc > 1)
+		while(argc > 1)
 		{
-			printf("Hello %s\n", agrv[argc-1]);
+			printf("Hello %s\n", argv[argc-1]);
 			argc--;
 		}
 	else
